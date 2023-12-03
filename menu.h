551577582ee08ff7b76914cb580d38d0fcae8030
aadilumair <aadilumair@protonmail.com>
2023-12-03 09:25:20 +0500

    made a high score area

--- menu.h
+++ menu.h
@@ -31,8 +31,34 @@ class Menu {
             if (Keyboard::isKeyPressed(Keyboard::E)) {
                 window.close();
             }
-            if (Keyboard::isKeyPressed(Keyboard::H)) {
-                std::cout << "H pressed";
+        }
+    }
+
+    void display_high_scores() {
+        bg_texture.loadFromFile("img/black.png");
+        background.setTexture(bg_texture);
+        background.setScale(1, 1);
+        RenderWindow window(VideoMode(780, 780), title);
+        while (window.isOpen()) {
+            Event e;
+            while (window.pollEvent(e))
+            {
+                if (e.type == Event::Closed) // If cross/close is clicked/pressed
+                    window.close(); //close the game                        	    
+            }
+            window.clear(Color::Black);
+            window.draw(background);
+            window.display();
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                window.close();
+                Game g;
+                g.start_game();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::B)) {
+                window.close();
+                display_menu();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::E)) {
                 window.close();
             }
         }
@@ -71,8 +97,8 @@ public:
                 window.close();
             }
             if (Keyboard::isKeyPressed(Keyboard::H)) {
-                std::cout << "H pressed";
                 window.close();
+                display_high_scores();
             }
         }
     }
