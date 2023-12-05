e047475c8f17b8461dace0251706470d73de4b6f
aadilumair <aadilumair@protonmail.com>
2023-12-05 20:04:06 +0500

    fixed maze unalignment and modifed car class a bit

--- car.h
+++ car.h
@@ -12,13 +12,14 @@ public:
 protected:
 	float speed;
 	Texture carTexture;
+	int currRing;
 };
 
 float Car::getSpeed() {
-
+	return speed;
 }
 void Car::setSpeed(float spd) {
-
+	speed = spd;
 }
 
 
@@ -87,7 +88,10 @@ class Enemy : Car
 {
 public:
 	Enemy(std::string png_path);
-	void move(std::string s);
+	void move();
+	void setMaxRing(int r);
+	void setCurrRing(int r);
+	void randChangeRing();
 
 private:
 	int maxRing;
@@ -97,20 +101,22 @@ Enemy::Enemy(std::string png_path)
 {
 	speed = 0.1;
 	maxRing = 0;
+	currRing = 0;
 	carTexture.loadFromFile(png_path);
 	car.setTexture(carTexture);
 	car.setPosition(340, 700);
 	car.setScale(0.75, 0.75);
 }
 
-void Enemy::move(std::string s) {
+void Enemy::move() {
 	float delta_x = 0, delta_y = 0;
+	string s = "l";
 	if (s == "l")
 		delta_x = -1;
-	//move the player left
+	//move the enemy left
 	else if (s == "r")
 		delta_x = 1;
-	//move the player right
+	//move the enemy right
 	if (s == "u")
 		delta_y = -1;
 	else if (s == "d")
@@ -121,4 +127,14 @@ void Enemy::move(std::string s) {
 
 	car.move(delta_x, delta_y);
 
+}
+
+void Enemy::setMaxRing(int r){
+	maxRing = r;
+}
+void Enemy::setCurrRing(int r){
+	maxRing = r;
+}
+void Enemy::randChangeRing(){
+	//if at turnPoint, then take random value form 0 to maxRing and change currRing to this value. Also move the car to that track
 }
