950f82c38f179077842b71e439a04c385d2dd94e
aadilumair <aadilumair@protonmail.com>
2023-12-09 11:30:14 +0500

    optimised Rings

--- car.h
+++ car.h
@@ -17,7 +17,7 @@ public:
 	float getSpeed();
 	void setSpeed(float spd);
 	Sprite car;
-	Ring R0, R1, R2, R3;
+	Ring R[4];
 
 protected:
 	float speed;
@@ -58,41 +58,41 @@ Player::Player(std::string png_path){
 
 	moveDir = 'r';
 
-	R0.A.x = 99;
-	R0.A.y = 99;
-	R0.B.x = 99;
-	R0.B.y = 634;
-	R0.C.x = 634;
-	R0.C.y = 634;
-	R0.D.x = 634;
-	R0.D.y = 99;
+	R[0].A.x = 99;
+	R[0].A.y = 99;
+	R[0].B.x = 99;
+	R[0].B.y = 634;
+	R[0].C.x = 634;
+	R[0].C.y = 634;
+	R[0].D.x = 634;
+	R[0].D.y = 99;
 
-	R1.A.x = 171;
-	R1.A.y = 171;
-	R1.B.x = 171;
-	R1.B.y = 560;
-	R1.C.x = 560;
-	R1.C.y = 560;
-	R1.D.x = 560;
-	R1.D.y = 171;
+	R[1].A.x = 171;
+	R[1].A.y = 171;
+	R[1].B.x = 171;
+	R[1].B.y = 560;
+	R[1].C.x = 560;
+	R[1].C.y = 560;
+	R[1].D.x = 560;
+	R[1].D.y = 171;
 
-	R2.A.x = 235;
-	R2.A.y = 235;
-	R2.B.x = 235;
-	R2.B.y = 498;
-	R2.C.x = 498;
-	R2.C.y = 498;
-	R2.D.x = 498;
-	R2.D.y = 235;
+	R[2].A.x = 235;
+	R[2].A.y = 235;
+	R[2].B.x = 235;
+	R[2].B.y = 498;
+	R[2].C.x = 498;
+	R[2].C.y = 498;
+	R[2].D.x = 498;
+	R[2].D.y = 235;
 
-	R3.A.x = 289;
-	R3.A.y = 289;
-	R3.B.x = 289;
-	R3.B.y = 440;
-	R3.C.x = 440;
-	R3.C.y = 440;
-	R3.D.x = 440;
-	R3.D.y = 289;
+	R[3].A.x = 289;
+	R[3].A.y = 289;
+	R[3].B.x = 289;
+	R[3].B.y = 440;
+	R[3].C.x = 440;
+	R[3].C.y = 440;
+	R[3].D.x = 440;
+	R[3].D.y = 289;
 
 
 
@@ -139,78 +139,26 @@ void Player::move(std::string s) {
 
 void Player::moveFwd() {
 	
-	if (currRing == 0) {
-		if ((car.getPosition().x == R0.A.x)&&(car.getPosition().y == R0.A.y))//corner A 
+	
+		cout << "";
+		if ((car.getPosition().x == R[currRing].A.x) && (car.getPosition().y == R[currRing].A.y))//corner A 
 		{
 			moveDir = 'd';//set movement to down
 		}
-		if ((car.getPosition().x == R0.B.x) && (car.getPosition().y == R0.B.y))//corner B
+		if ((car.getPosition().x == R[currRing].B.x) && (car.getPosition().y == R[currRing].B.y))//corner B
 		{
 			moveDir = 'r';//set movement to right
 		}
-		if ((car.getPosition().x == R0.C.x) && (car.getPosition().y == R0.C.y))//corner C
+		if ((car.getPosition().x == R[currRing].C.x) && (car.getPosition().y == R[currRing].C.y))//corner C
 		{
 			moveDir = 'u';//set movement to up
 		}
-		if ((car.getPosition().x == R0.D.x) && (car.getPosition().y == R0.D.y))//corner D
+		if ((car.getPosition().x == R[currRing].D.x) && (car.getPosition().y == R[currRing].D.y))//corner D
 		{
 			moveDir = 'l';//set movement to left
 		}
-	}
-	else if (currRing == 1) {
-		if ((car.getPosition().x == R1.A.x) && (car.getPosition().y == R1.A.y))//corner A 
-		{
-			moveDir = 'd';//set movement to down
-		}
-		if ((car.getPosition().x == R1.B.x) && (car.getPosition().y == R1.B.y))//corner B
-		{
-			moveDir = 'r';//set movement to right
-		}
-		if ((car.getPosition().x == R1.C.x) && (car.getPosition().y == R1.C.y))//corner C
-		{
-			moveDir = 'u';//set movement to up
-		}
-		if ((car.getPosition().x == R1.D.x) && (car.getPosition().y == R1.D.y))//corner D
-		{
-			moveDir = 'l';//set movement to left
-		}
-	}
-	else if (currRing == 2) {
-		if ((car.getPosition().x == R2.A.x) && (car.getPosition().y == R2.A.y))//corner A 
-		{
-			moveDir = 'd';//set movement to down
-		}
-		if ((car.getPosition().x == R2.B.x) && (car.getPosition().y == R2.B.y))//corner B
-		{
-			moveDir = 'r';//set movement to right
-		}
-		if ((car.getPosition().x == R2.C.x) && (car.getPosition().y == R2.C.y))//corner C
-		{
-			moveDir = 'u';//set movement to up
-		}
-		if ((car.getPosition().x == R2.D.x) && (car.getPosition().y == R2.D.y))//corner D
-		{
-			moveDir = 'l';//set movement to left
-		}
-	}
-	else if (currRing == 3) {
-		if ((car.getPosition().x == R3.A.x) && (car.getPosition().y == R3.A.y))//corner A 
-		{
-			moveDir = 'd';//set movement to down
-		}
-		if ((car.getPosition().x == R3.B.x) && (car.getPosition().y == R3.B.y))//corner B
-		{
-			moveDir = 'r';//set movement to right
-		}
-		if ((car.getPosition().x == R3.C.x) && (car.getPosition().y == R3.C.y))//corner C
-		{
-			moveDir = 'u';//set movement to up
-		}
-		if ((car.getPosition().x == R3.D.x) && (car.getPosition().y == R3.D.y))//corner D
-		{
-			moveDir = 'l';//set movement to left
-		}
-	}
+	
+	
 
 	if (moveDir == 'u') {
 		car.move(0, -1 * speed);
